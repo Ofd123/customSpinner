8ef4f75cb9685dd7f20181712bff183c1fc47829
ad6354 <ad6354@bs.amalnet.k12.il>
2024-12-12 21:16:59 +0200

    finished

--- app/src/main/java/com/example/customlisthw/MainActivity.java
+++ app/src/main/java/com/example/customlisthw/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.customlisthw;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,11 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     Spinner spin;
     TextView countryName,capitalCity,population;
     ImageView flag;
+    int [] imgs = {R.drawable.uk_flag,R.drawable.usa_flag,R.drawable.ukrine_flag,R.drawable.germany_flag,R.drawable.india_flag,R.drawable.israel_flag,R.drawable.italy_flag,R.drawable.russia_flag};
+    String [] capitalCities = {"London","Washington, D.C.","Kyiv","Berlin","New Delhi","Jerusalem","Rome","Moscow"};
+    String [] countries = {"United Kingdom","United States","Ukraine","Germany","India","Israel","Italy","Russia"};
+    int [] populations = {69140000, 334200000, 31400000 , 83400000, 1425000000, 9500000, 58900000, 143800000};
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +36,37 @@ public class MainActivity extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        countryName = findViewById(R.id.country);
+        capitalCity = findViewById(R.id.capitalCity);
+        population = findViewById(R.id.population);
+        flag = findViewById(R.id.flag);
 
+
+        //-------------------------------------------
+        spin = (Spinner) findViewById(R.id.spinner);
+
+        spinneradapter custom = new spinneradapter(this,imgs,countries);
+        spin.setAdapter(custom);
+        spin.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        countryName.setText(countries[position]);
+        capitalCity.setText("Capital: " + capitalCities[position]);
+        population.setText("Population: " + populations[position]);
+        flag.setImageResource(imgs[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        countryName.setText(countries[0]);
+        capitalCity.setText("Capital: " + capitalCities[0]);
+        population.setText("Population: " + populations[0]);
+        flag.setImageResource(imgs[0]);
     }
 }
--- app/src/main/java/com/example/customlisthw/spinneradapter.java
+++ app/src/main/java/com/example/customlisthw/spinneradapter.java
@@ -1,6 +1,61 @@
 package com.example.customlisthw;
 
-public class spinneradapter
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+public class spinneradapter extends BaseAdapter
 {
-    
+    SpinnerAdapter spinadp;
+    private Context context;
+    private int[] imgs;
+    private String[] countries;
+    private LayoutInflater inflater;
+
+    public spinneradapter(Context context,int[] imgs, String[] countries)
+    {
+        this.context = context;
+        this.imgs = imgs;
+        this.countries = countries;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount()
+    {
+        return countries.length;
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return countries[position];
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent)
+    {
+        view = inflater.inflate(R.layout.spinneradapter, parent,false);
+        ImageView img = view.findViewById(R.id.countryImages);
+        TextView tv = (TextView) view.findViewById(R.id.countryNameText);
+        tv.setText(countries[position]);
+        img.setImageResource(imgs[position]);
+        //-----------------------------------------
+
+
+        return view;
+
+
+    }
 }
